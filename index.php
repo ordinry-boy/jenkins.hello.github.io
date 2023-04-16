f41e70f17e39f609c42a71bc0de140aaec130be1
Aditya Sahu <119127013+ordinry-boy@users.noreply.github.com>
2023-04-16 21:18:53 +0530

    Update index.php
    testing

--- index.php
+++ index.php
@@ -1,11 +1,22 @@
-<?php
-
-// Set the content type to text/html
-
-header('Content-Type: text/html');
-
-// display "Hello world!"
-
-echo "Hello world!";
-
-?>
+<html>
+<head>
+<title>Computer</title>
+</head>
+<body bgcolor="red">
+<center>
+<h3 alignment="center">Types of computer
+</h3>
+</center><br>
+<center>
+<img src="com.jpg" height="30%" width="20%">
+</center>
+<p>The different types of computers are </p>
+<p>a Supercomputers<br>
+b Main frame computers<br>
+c Mini computers<br>
+d desktops</p>
+<p>Types of memory<br>
+i primary memory<br>
+ii secondary memory <br></p>
+</body> 
+</html>
